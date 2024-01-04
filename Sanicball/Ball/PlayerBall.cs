f3296a2bca6cc243df72cb2cfeaf9f8473edf5ab
ArchercatNEO <tururu.pompella@gmail.com>
2024-01-04 19:45:24 +0000

    Made a bad cam follow

--- Sanicball/Ball/PlayerBall.cs
+++ Sanicball/Ball/PlayerBall.cs
@@ -1,10 +1,18 @@
 using Godot;
 using Sanicball.Account;
+using Sanicball.Scenes;
 
 namespace Sanicball.Ball;
 
 public partial class PlayerBall : AbstractBall
 {
+    public override void _Ready()
+    {
+        base._Ready();
+
+        LobbyCamera.TrySubscribe(this);
+    }
+
     public override void _Input(InputEvent @event)
     {
         base._Input(@event);
