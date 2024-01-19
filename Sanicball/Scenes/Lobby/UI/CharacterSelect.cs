fae6e0e023fc2a81804436946b612d276d99f285
ArchercatNEO <tururu.pompella@gmail.com>
2024-01-19 17:02:47 +0000

    Converted photoshop files to png

--- Sanicball/Scenes/Lobby/UI/CharacterSelect.cs
+++ Sanicball/Scenes/Lobby/UI/CharacterSelect.cs
@@ -4,5 +4,10 @@ namespace Sanicball.Scenes;
 
 public partial class CharacterSelect : Control
 {
-
+    private static readonly PackedScene prefab = GD.Load<PackedScene>("res://Scenes/Lobby/UI/CharacterSelect.tscn");
+    
+    public static CharacterSelect Create()
+    {
+        return prefab.Instantiate<CharacterSelect>();
+    }
 }
