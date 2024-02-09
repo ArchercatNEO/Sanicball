51d881a30d687c5060e954bcbb65b7ca80c472e9
ArchercatNEO <tururu.pompella@gmail.com>
2024-02-09 19:02:18 +0000

    Break everything

--- +NullableExports/Program.cs
+++ +NullableExports/Program.cs
@@ -1,102 +1,87 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections.Immutable;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading;
 using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
 
-namespace SourceGenerator;
+namespace NullableExports;
 
-[Generator]
-public class HelloSourceGenerator : ISourceGenerator
+struct FileWithImport
 {
-    public void Execute(GeneratorExecutionContext context)
-    {
-        var nullableContainingTypes = context.Compilation.Assembly.Modules
-            .Select(module => module.GlobalNamespace)
-            .SelectMany(rootNamespace => rootNamespace.GetTypeMembers())
-            .Where(type => type.GetMembers().Where(member => 
-                member.GetAttributes().Any(attribute => attribute.AttributeClass!.Name == "ExportAttribute")
-            ).Any());
-
-        foreach (var nullableType in nullableContainingTypes)
-        {
-            StringBuilder configurationWarnings = new();
-
-            configurationWarnings.Append($@"
-                public partial class {nullableType.Name}
-                {{
-            ");
-            
-            configurationWarnings.Append("""
-            public override string[] _GetConfigurationWarnings()
-            {
-                List<string> warnings = ["Bad"];
-            """);
-                
-
-            configurationWarnings.Append(" return warnings.ToArray();} } ");
-                
-            
-
-
-            context.AddSource(nullableType.Name + ".g.cs", configurationWarnings.ToString());
-        }
-    }
-
-    public void Initialize(GeneratorInitializationContext context)
-    {
-        // No initialization required for this one
-    }
+    public AdditionalText data;
+    public AdditionalText import;
 }
-
-[Generator]
-public class NullableExportsGenerator : IIncrementalGenerator
-{
-    public void Initialize(IncrementalGeneratorInitializationContext context)
-    {
-        var project = context.AdditionalTextsProvider.Where(static file => {
-            Console.WriteLine("hello");
-            Console.WriteLine(file);
-            return true;
-        });
-
-        context.RegisterSourceOutput(project, (source, context) => {
-            StringBuilder builder = new();
-            builder.Append("""
-            namespace Sanicball;
-
-            public class GodotActions
-            {
-                public static readonly world = "hello"
-            }
-            """);
-            source.AddSource("Project.cs", builder.ToString());
-            DiagnosticDescriptor error = new("SB0000", "No null required fields", "hello", "security", DiagnosticSeverity.Error, true);
-            Diagnostic diagnostic = Diagnostic.Create(error, null);
-            source.ReportDiagnostic(diagnostic);
-        });
-    }
-}
-
-[AttributeUsage(AttributeTargets.Field)]
-public class PreloadAttribute(string _path) : Attribute
-{
-}
-
 
 [Generator]
 public class PreloadGenerator : IIncrementalGenerator
 {
     public void Initialize(IncrementalGeneratorInitializationContext context)
     {
-        context.AdditionalTextsProvider
-            .Where(file => true) //Extension is relevant
-            .Select((file, token) => file); //Merge into importable file
+        var resourceData = context.AdditionalTextsProvider.Where(file => !file.Path.EndsWith(".tres") && !file.Path.EndsWith(".tscn"));
+        var resouceImports = resourceData.Collect().SelectMany((files, token) => {
+            List<FileWithImport> importers = [];
+            Dictionary<string, AdditionalText> pendingImports = [];
+            Dictionary<string, AdditionalText> pendingData = [];
+            foreach (var file in files)
+            {
+                if (file.Path.EndsWith(".import"))
+                {
+                    string dataPath = Path.GetFileNameWithoutExtension(file.Path);
+                    if (!pendingData.ContainsKey(dataPath))
+                    {
+                        pendingImports.Add(file.Path, file);
+                        continue;
+                    }
+                    importers.Add(new()
+                    {
+                        data = pendingData[dataPath],
+                        import = file
+                    });
+                    pendingData.Remove(dataPath);
+                }
+                else
+                {
+                    string importPath = file.Path + ".import";
+                    if (!pendingImports.ContainsKey(importPath))
+                    {
+                        pendingData.Add(file.Path, file);
+                        continue;
+                    }
+                    importers.Add(new()
+                    {
+                        data = file,
+                        import = pendingImports[importPath]
+                    });
+                    pendingImports.Remove(importPath);
+                }
+            }
+            return importers;
+        }).Select((file, token) => {
+            //Import the file
+            return file.data;
+        }).Collect().Select((files, token) => {
+            Dictionary<string, AdditionalText> lookupTable = [];
+            foreach (var file in files)
+            {
+                lookupTable.Add(file.Path, file);
+            }
+            return lookupTable;
+        });
+        
+        var resourceFiles = context.AdditionalTextsProvider.Where(file => file.Path.EndsWith(".tres") || file.Path.EndsWith(".tscn")).Collect();
+        var resourceTree = resouceImports.Combine(resourceFiles).Select((files, token) => {
+            Dictionary<string, AdditionalText> tree = files.Left;
+            foreach (var file in files.Right)
+            {
+                tree.Add(file.Path, file);
+            }
+            return tree;
+        });
+       
         var syntax = context.SyntaxProvider
-            .ForAttributeWithMetadataName("SourceGenerator.PreloadAttribute", SyntaxFilter, SyntaxTransformer)
+            .ForAttributeWithMetadataName("Sanicball.PreloadAttribute", SyntaxFilter, SyntaxTransformer)
             .Collect()
             .SelectMany((fields, token) => {
                 Dictionary<string, ClassAndFields> sorted = [];
@@ -117,8 +102,13 @@ public class PreloadGenerator : IIncrementalGenerator
                     parent.fields.Add(field);
                 }
                 return sorted.Values;
-            });
-        context.RegisterSourceOutput(syntax, (ctx, classData) => {
+            }).Combine(resourceTree);
+        context.RegisterSourceOutput(syntax, (ctx, compilationContext) => {
+            var (classData, resourceTree) = compilationContext;
+
+            int files = resourceTree.Count;
+            string avalible = string.Join("\n", resourceTree.Select(res => res.Key));
+
             StringBuilder builder = new();
             builder.Append($$"""
             namespace Sanicball.{{classData.containingNamespace}};
@@ -131,7 +121,23 @@ public class PreloadGenerator : IIncrementalGenerator
 
             foreach (var field in classData.fields)
             {
-                builder.Append($"{field.fieldName} = new();");
+                if (resourceTree.ContainsKey(field.path))
+                {
+                    builder.Append($"{field.fieldName} = Godot.GD.Load<{field.fieldType}>(\"{field.path}\");");
+                    continue;
+                }
+
+                DiagnosticDescriptor description = new(
+                    "GD0000",
+                    "Invalid Resource File Path",
+                    $"File path: {field.path} does not exist, {avalible} files exist",
+                    "Compilation",
+                    DiagnosticSeverity.Error,
+                    true,
+                    $"File path: {field.path} does not exist, {avalible} files exist"
+                );
+                Diagnostic invalidPath = Diagnostic.Create(description, field.location);
+                ctx.ReportDiagnostic(invalidPath);
             }
 
             builder.Append("""
@@ -140,6 +146,18 @@ public class PreloadGenerator : IIncrementalGenerator
             """);
 
             ctx.AddSource($"{classData.className}.g.cs", builder.ToString());
+        });
+
+        context.RegisterPostInitializationOutput(ctx => {
+            ctx.AddSource("PreloadAttribute.g.cs", @"
+                namespace Sanicball;
+                #pragma warning disable CS9113 // Parameter is unread.
+                [System.AttributeUsage(System.AttributeTargets.Field)]
+                public class PreloadAttribute(string path) : System.Attribute
+                {
+                }
+                #pragma warning restore CS9113 // Parameter is unread.
+            ");
         });
     }
 
@@ -153,7 +171,9 @@ public class PreloadGenerator : IIncrementalGenerator
         return new(){
             containingNamespace = context.TargetSymbol.ContainingNamespace.Name,
             className = context.TargetSymbol.ContainingType.Name,
+            location = context.TargetNode.GetLocation(),
             fieldName = context.TargetSymbol.Name,
+            fieldType = ((IFieldSymbol)context.TargetSymbol).Type!.Name,
             path = (string)context.Attributes[0].ConstructorArguments[0].Value!
         };
     }
@@ -171,6 +191,8 @@ internal struct FieldDescriptor
     public string containingNamespace;
     public string className;
 
+    public Location location;
     public string fieldName;
+    public string fieldType;
     public string path;
 }
