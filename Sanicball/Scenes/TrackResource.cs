b9ac86054743b255b291a07aadf08515a73d2891
ArchercatNEO <tururu.pompella@gmail.com>
2024-03-27 17:51:31 +0000

    Add documentation to track representation and make zones 1+2 conform to it

--- Sanicball/Scenes/TrackResource.cs
+++ Sanicball/Scenes/TrackResource.cs
@@ -2,10 +2,21 @@ using Godot;
 
 namespace Sanicball.Scenes;
 
+/// <summary>
+/// A resource to represent what a stage track is and does
+/// </summary>
+[GlobalClass]
 public partial class TrackResource : Resource
 {
     [Export] public required string name;
     [Export] public required PackedScene prefab;
     [Export] public required Texture2D flatOverlay;
     [Export] public required PackedScene solidOverlay;
+
+    public void Activate(SceneTree tree)
+    {
+        tree.ChangeSceneToPacked(prefab);
+    }
+
+    public static readonly TrackResource GreenHillZone = GD.Load<TrackResource>("res://Scenes/Z01-GreenHillZone/GreenHillZone.tres");
 }
