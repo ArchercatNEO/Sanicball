7983be8e57b9f113535cadee918cb825de39db05
ArchercatNEO <tururu.pompella@gmail.com>
2024-03-27 17:35:41 +0000

    Add documentation and a scaffold for network types

---
+++ Sanicball/accounts/Player.cs
@@ -0,0 +1,10 @@
+namespace Sanicball.Account;
+
+/// <summary>
+/// Describes a single player by its client's GUID and the ControlType of the player
+/// The combination of the two should provide a unique identifier for a player
+/// </summary>
+public class Player
+{
+
+}
