ed7efae0a764842cead54a432b91b1dccc7acd59
ArchercatNEO <tururu.pompella@gmail.com>
2024-04-20 10:36:59 +0100

    Update the flakes to still not be useful

--- flake.nix
+++ flake.nix
@@ -1,27 +1,29 @@
 {
-  description = "The sanicball dev environment";
+    description = "The sanicball dev environment";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
-  };
-
-  outputs = { self, nixpkgs }:
-  let 
-    system = "x86_64-linux"; 
-    pkgs = import nixpkgs {
-        inherit system;
-      };
-  in {
-    devShells."${system}".default = pkgs.mkShell {
-      
-      packages = with pkgs; [
-        dotnet-sdk_8
-        godot_4
-      ];
-
-      shellHook = ''
-        dotnet ${pkgs.dotnet-sdk_8}/bin/dotnet --version
-      '';
+    inputs = {
+        nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+        nixunstable.url = "github:nixos/nixpkgs/nixos-unstable";
     };
-  };
+
+    outputs = { self, nixpkgs, nixunstable }:
+    let
+        system = "x86_64-linux";
+        pkgs = import nixunstable {
+            inherit system;
+        };
+    in {
+
+        packages."${system}".default = pkgs.mkPackage {
+
+        };
+
+        devShells."${system}".default = pkgs.mkShell {
+
+            packages = with pkgs; [
+                dotnet-sdk_8
+                godot_4
+            ];
+        };
+    };
 }
