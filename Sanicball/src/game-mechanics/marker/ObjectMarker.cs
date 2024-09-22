2b6d58f49a6eba752059e81da07736ff1989d930
ArchercatNEO <tururu.pompella@gmail.com>
2024-09-22 15:07:15 +0100

    Remove all remaining references to res://src/ball and SanicBall

--- Sanicball/src/game-mechanics/marker/ObjectMarker.cs
+++ Sanicball/src/game-mechanics/marker/ObjectMarker.cs
@@ -6,7 +6,7 @@ namespace Sanicball.Ball;
 [GodotClass]
 public partial class ObjectMarker : Control
 {
-    private static readonly PackedScene prefab = GD.Load<PackedScene>("res://src/ball/ObjectMarker.tscn");
+    private static readonly PackedScene prefab = GD.Load<PackedScene>("res://src/game-mechanics/marker/ObjectMarker.tscn");
 
     /// <summary>
     /// Factory method
